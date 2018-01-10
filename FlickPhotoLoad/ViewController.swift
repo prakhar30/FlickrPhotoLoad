b2cb48379a29dc866176648d6336b9ad25bcb9f0
prakhar30 <prakhar.chanu30@gmail.com>
2018-01-10 19:29:54 +0530

    first commit of full project folder

---
+++ FlickPhotoLoad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlickPhotoLoad
+//
+//  Created by Prakhar Tripathi on 10/01/18.
+//  Copyright © 2018 Prakhar Tripathi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
